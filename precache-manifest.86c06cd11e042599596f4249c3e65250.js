c6f79f1f12f5bfa2f211ae8ccad5c2c37c138d9d
Ammko128 <ammar.veljagic@gmail.com>
2020-09-10 22:32:43 +0200

    Updates

---
+++ precache-manifest.86c06cd11e042599596f4249c3e65250.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bbcf607f2bdce69a9022e82e1a9dd89",
+    "url": "/snake-game/index.html"
+  },
+  {
+    "revision": "9ce3c697a899384a961c",
+    "url": "/snake-game/static/css/main.22a7839a.chunk.css"
+  },
+  {
+    "revision": "3c81687dc08d4f413189",
+    "url": "/snake-game/static/js/2.beaa076a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake-game/static/js/2.beaa076a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ce3c697a899384a961c",
+    "url": "/snake-game/static/js/main.ce04d0a5.chunk.js"
+  },
+  {
+    "revision": "18005e22df855f84fc12",
+    "url": "/snake-game/static/js/runtime-main.f9e1bf59.js"
+  }
+]);
